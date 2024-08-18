9d6bcfc013fd81fbb1f7614e843d21d6d1f9bb23
Yash Kothari <yashk8119@gmail.com>
2024-08-18 22:20:48 +0530

    Client Setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import Routes from "./comp/Routes/Routes.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={Routes} />
+);
